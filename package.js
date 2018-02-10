267f25c3d7e696ffc585c521f65dd0819cd88e1e
Vercryger <abuzaid.karim@gmail.com>
2018-02-10 09:26:45 -0300

    Fix GitHub hyperlink

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'qactivo:meteor-tables',
   summary: 'Reactive tables for large or small datasets in Meteor',
-  git: 'https://github.com/QActivo/meteor-tables.git',
+  git: 'https://github.com/toptive/meteor-tables.git',
   version: '1.1.7'
 });
 
